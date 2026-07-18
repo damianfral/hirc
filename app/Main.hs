1d9c4289fb759d0eaa605dc5cdf614d5a7b14965
damianfral <huevofritopamojarpan@gmail.com>
2026-07-18 23:36:52 +0000

    Add app.

---
+++ app/Main.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Main where
+
+import IRC.CLI
+import Relude
+
+main :: IO ()
+main = runCLI
---
+++ src/IRC/CLI.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module IRC.CLI where
+
+import Data.Version (showVersion)
+import IRC.Client
+import IRC.Domain
+import IRC.Protocol
+import IRC.UI (runUI)
+import Options.Generic
+import Paths_hirc (version)
+import Relude
+
+data Options w = Options
+  { nickname :: w ::: Text,
+    username :: w ::: Text,
+    realname :: w ::: Text,
+    host :: w ::: String <!> "irc.libera.chat" <?> "IRC server hostname",
+    port :: w ::: String <!> "6667" <?> "IRC server port"
+  }
+  deriving (Generic)
+
+instance ParseRecord (Options Wrapped) where
+  parseRecord = parseRecordWithModifiers lispCaseModifiers
+
+runCLI :: IO ()
+runCLI = do
+  unwrapRecord msg >>= runCLIOptions
+  exitSuccess
+  where
+    msg = unwords ["hirc", "v" <> show (showVersion version)]
+
+runCLIOptions :: Options Unwrapped -> IO ()
+runCLIOptions Options {..} =
+  withIRCClient host port $ \client -> do
+    writeAction client
+      $ Register (Nickname nickname) (Username username) (Realname realname)
+    runUI client (Nickname nickname)
